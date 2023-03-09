4556f8d723c6973e77e8a090fc6df99f4ffa6d56
EWECORP <110986865+EWECORP@users.noreply.github.com>
2023-03-09 11:49:36 -0300

    con comentario

--- Inicio.py
+++ Inicio.py
@@ -1,3 +1,12 @@
 # Esto es una prueba nada mas "
 
 print('Primera Prueba')
+
+
+# echo "# FLASK" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/EWEARG/FLASK.git
+# git push -u origin main
